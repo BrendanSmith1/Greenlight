435c71929558c5700eb8132a7513a6f0a8e1124b
BrendanSmith <brendansmith464@gmail.com>
2024-02-05 16:51:39 +0800

    Basic CRUD actions set-up for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,43 @@
 class ProjectsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
+    @projects = Project.all
   end
 
   def show
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.save
+    redirect_to project_path(@project)
+  end
+
+  def edit
+  end
+
+  def update
+    @project.update(project_params)
+    redirect_to project_path(@project)
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path, status: :see_other
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :address, :company)
+  end
+
+  def set_restaurant
+    @project = Project.find(params[:id])
   end
 end
