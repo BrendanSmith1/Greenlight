12431ec4fea638b81ea1ea8994929ba8ba1d8328
BrendanSmith <brendansmith464@gmail.com>
2024-02-08 13:21:06 +0800

    Added authentication to all pages execpt home page, added more attributes to User model and sign-up and edit forms

---
+++ db/migrate/20240208050543_add_namesto_users.rb
@@ -0,0 +1,8 @@
+class AddNamestoUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :job_title, :string
+    add_column :users, :company, :string
+  end
+end
