20609b75c76b9c2c37954e4bb689ce110a66bace
BrendanSmith <brendansmith464@gmail.com>
2024-03-09 16:04:35 +0800

    Added drawing dependency to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-  has_many :drawings
+  has_many :drawings, dependent: :destroy
   belongs_to :user
   has_many :project_users
   has_many :users, through: :project_users
