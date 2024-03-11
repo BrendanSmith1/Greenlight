e4ade6701aefb2cdff49bb340de9ba0637e3add1
BrendanSmith <brendansmith464@gmail.com>
2024-03-11 15:03:31 +0800

    Able to add a new drawing

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -1,2 +1,43 @@
 class DrawingsController < ApplicationController
+
+  def new
+    @drawing = Drawing.new
+    # authorize @drawing
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @drawing = Drawing.new(drawing_params)
+    @drawing.project = @project
+    # authorize @drawing
+    if @drawing.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @drawing = Drawing.find(params[:id])
+    @project = Project.find(params[:project_id])
+    # authorize @drawing
+  end
+
+  def update
+    @drawing = Drawing.find(params[:id])
+    @drawing.update(drawing_params)
+    redirect_to project_path(@drawing.project)
+  end
+
+  def destroy
+    @drawing = Drawing.find(params[:id])
+    @drawing.destroy
+    redirect_to project_path(@drawing.project)
+  end
+
+  private
+
+  def drawing_params
+    params.require(:drawing).permit(:name, :number, :revision)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  #index, create, new, edit, show, update, destroy
+  # index, create, new, edit, show, update, destroy
 
   # Devise scope for authenticated and unauthenticated users?
   root to: "pages#home"
-  resources :projects, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :projects, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :drawings, only: [:new, :create, :edit, :update, :destroy]
+  end
 end
